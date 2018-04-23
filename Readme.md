a7bf791cbab5266d80b0618d48ed0a6f4dcc8b98
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to enable Drag-and-Drop feature in the CardView
+
+
+<p>This example shows how to implement the <a href="https://documentation.devexpress.com/WPF/11346/Controls-and-Libraries/Data-Grid/Drag-and-Drop">Drag-and-Drop</a> functionality in the CardView.</p>
+<p>This functionality is encapsulated in the CardDragDropManager class. So, all you need to do is to attach this behavior to the GridControl.<br>Starting from version <strong>17.2</strong>, the GridControl ships with native Drag-and-Drop support.</p>
+
+<br/>
+
+
---
+++ VB/Application.xaml.vb
@@ -0,0 +1,41 @@
+﻿' Developer Express Code Central Example:
+' How to implement the Drag&Drop functionality for the CardView
+' 
+' We have created an example demonstrating how to implement the Drag&Drop
+' functionality for the CardView.
+' This functionality is encapsulated in the
+' CardDragDropManager class. So, all you need to do is to attach this behavior to
+' the GridControl.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4616
+
+' Developer Express Code Central Example:
+' How to implement the Drag&Drop functionality for the CardView
+' 
+' We have created an example demonstrating how to implement the Drag&Drop
+' functionality for the CardView.
+' This functionality is encapsulated in the
+' CardDragDropManager class. So, all you need to do is to attach this behavior to
+' the GridControl.
+' 
+' You can find sample updates and versions for different programming languages here:
+' http://www.devexpress.com/example=E4616
+
+Imports System
+Imports System.Collections.Generic
+Imports System.Configuration
+Imports System.Data
+Imports System.Linq
+Imports System.Threading.Tasks
+Imports System.Windows
+
+Namespace CardViewDragDrop
+    ''' <summary>
+    ''' Interaction logic for App.xaml
+    ''' </summary>
+    Partial Public Class App
+        Inherits Application
+
+    End Class
+End Namespace
